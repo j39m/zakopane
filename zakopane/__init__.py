50d35de34bf6c2f90d0b8a4595c6d1459355d8fa
j39m <hikaph@gmail.com>
2017-03-12 23:47:39 -0700

    Expand package and module extents.

--- zakopane/__init__.py
+++ zakopane/__init__.py
@@ -2,4 +2,13 @@ version = (0, 0, 1)
 
 DEBUG = False
 
-from zakopane.file import SumFile
+
+import os
+from zakopane.file import SumFile, newSumFile
+from zakopane.hash import readHashLine, doHashFile, formatHashLine
+
+def main():
+    if "POSIX_ME_HARDER" in os.environ:
+        raise OSError("POSIXing you harder.")
+
+    return 0
