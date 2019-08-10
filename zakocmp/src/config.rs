9c371cbeffd1a51bd7ab01165e2645f4a5e65d5f
j39m <hikaph@gmail.com>
2019-08-09 22:51:39 -0700

    zakocmp: add documentation tests for config module
    This change
    *   converts the comments on the public config API to rustdocs,
    *   adds unit tests policy_repr_as_int(), and
    *   corrects a bug in the initial definition of POLICY_IMMUTABLE.

--- zakocmp/src/config.rs
+++ zakocmp/src/config.rs
@@ -16,7 +16,7 @@ pub const POLICY_IGNORE: i32 = 0;
 pub const POLICY_NOADD: i32 = 1 << 0;
 pub const POLICY_NODELETE: i32 = 1 << 1;
 pub const POLICY_NOMODIFY: i32 = 1 << 2;
-pub const POLICY_IMMUTABLE: i32 = POLICY_NOADD & POLICY_NODELETE & POLICY_NOMODIFY;
+pub const POLICY_IMMUTABLE: i32 = POLICY_NOADD | POLICY_NODELETE | POLICY_NOMODIFY;
 
 // Borrows the string representation of one policy `token` and returns
 // the equivalent integral representation.
@@ -34,9 +34,67 @@ fn policy_token_as_int(token: &str) -> Result<i32, Error> {
     }
 }
 
-// Borrows the string representation of a combined `policy` and returns
-// the equivalent integral representation. This function expects
-// `policy` to comprise one or more policy tokens separated by commas.
+/// Borrows the string representation of a combined `policy` and returns
+/// the equivalent integral representation. This function expects
+/// `policy` to comprise one or more policy tokens separated by commas.
+///
+/// # Examples
+///
+/// ```
+/// # mod zakocmp;
+/// let policy: i32 = config::policy_repr_as_int(&"immutable").unwrap();
+/// assert!(policy == config::POLICY_IMMUTABLE);
+/// ```
+///
+/// ```
+/// # mod zakocmp;
+/// let policy: i32 = config::policy_repr_as_int(&"noadd").unwrap();
+/// assert!(policy == config::POLICY_NOADD);
+/// ```
+///
+/// ```
+/// # mod zakocmp;
+/// let policy: i32 = config::policy_repr_as_int(&"nomodify").unwrap();
+/// assert!(policy == config::POLICY_NOMODIFY);
+/// ```
+///
+/// ```
+/// # mod zakocmp;
+/// let policy: i32 = config::policy_repr_as_int(&"nodelete").unwrap();
+/// assert!(policy == config::POLICY_NODELETE);
+/// ```
+///
+/// ```
+/// # mod zakocmp;
+/// // While it's pointless to repeat oneself,
+/// // it's not invalid to do so.
+/// let policy: i32 = config::policy_repr_as_int(
+///     &"immutable,immutable,immutable").unwrap();
+/// assert!(policy == config::POLICY_IMMUTABLE);
+/// ```
+///
+/// ```
+/// # mod zakocmp;
+/// let policy: i32 = config::policy_repr_as_int(
+///     &"noadd,nodelete,nomodify").unwrap();
+/// assert!(policy == config::POLICY_IMMUTABLE);
+/// ```
+///
+/// ```
+/// # mod zakocmp;
+/// let policy: i32 = config::policy_repr_as_int(
+///     &"noadd,nomodify").unwrap();
+/// assert!(policy == config::POLICY_NOADD | config::POLICY_NOMODIFY);
+/// ```
+///
+/// # Failures
+///
+/// ```
+/// # mod zakocmp;
+/// // Obi-wan was witty, but not good at dictating zakocmp policy.
+/// assert!(!config::policy_repr_as_int(&"hello there!").is_ok());
+/// ```
+///
 pub fn policy_repr_as_int(policy: &str) -> Result<i32, Error> {
     let mut policy_int: i32 = POLICY_IGNORE;
     for token in policy.split(",") {
