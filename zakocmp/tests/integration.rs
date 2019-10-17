3b8a62d5fe98d2c8da09a9f54e214eed490dd904
j39m <hikaph@gmail.com>
2019-10-16 20:51:03 -0700

    zakocmp: add involved integration test

--- zakocmp/tests/integration.rs
+++ zakocmp/tests/integration.rs
@@ -95,3 +95,59 @@ fn test_basic_default_immutability() {
         the_same_shifty_violations.to_string()
     );
 }
+
+#[test]
+fn test_overlapping_prefixes() {
+    let config = Config::new(
+        r#"
+    default-policy: immutable
+    policies:
+        ./Pictures/: immutable
+        ./Pictures/2020/: ignore
+        ./Documents/: nodelete
+        "#,
+    )
+    .unwrap();
+    let snapshot_older = Snapshot::new(&snapshot_string_for_testing(indoc!(
+        r#"
+        0000000000000000000000000000000000000000000000000000000000000000  ./Music/hello-there.mp3
+        0000000000000000000000000000000000000000000000000000000000000000  ./Pictures/general-kenobi.gif
+        0000000000000000000000000000000000000000000000000000000000000000  ./Pictures/2020/you-are.gif
+        0000000000000000000000000000000000000000000000000000000000000000  ./Pictures/2020/a-bold-one.gif
+        0000000000000000000000000000000000000000000000000000000000000000  ./Pictures/2019/something-immutable.jpg
+        0000000000000000000000000000000000000000000000000000000000000000  ./Pictures/2019/something-supposedly-immutable.jpg
+        0000000000000000000000000000000000000000000000000000000000000000  ./Documents/nodelete-1.txt
+        0000000000000000000000000000000000000000000000000000000000000000  ./Documents/nodelete-2.txt
+        "#
+    )))
+    .unwrap();
+    let snapshot_newer = Snapshot::new(&snapshot_string_for_testing(indoc!(
+        r#"
+        ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff  ./Music/hello-there.mp3
+        0000000000000000000000000000000000000000000000000000000000000000  ./Music/copy-of-hello-there.mp3
+        ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff  ./Pictures/general-kenobi.gif
+        0000000000000000000000000000000000000000000000000000000000000000  ./Pictures/copy-of-general-kenobi.gif
+        0000000000000000000000000000000000000000000000000000000000000000  ./Pictures/2020/copy-of-you-are.gif
+        ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff  ./Pictures/2020/a-bold-one.gif
+        0000000000000000000000000000000000000000000000000000000000000000  ./Pictures/2019/something-immutable.jpg
+        ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff  ./Pictures/2019/something-supposedly-immutable.jpg
+        ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff  ./Documents/nodelete-2.txt
+        "#
+    )))
+    .unwrap();
+
+    let violations = libzakocmp::enter(&config, &snapshot_older, &snapshot_newer);
+    assert_eq!(
+        violations.to_string(),
+        indoc!(
+            r#"
+            - ./Documents/nodelete-1.txt
+            + ./Music/copy-of-hello-there.mp3
+            ! ./Music/hello-there.mp3
+            ! ./Pictures/2019/something-supposedly-immutable.jpg
+            + ./Pictures/copy-of-general-kenobi.gif
+            ! ./Pictures/general-kenobi.gif
+            "#
+        )
+    );
+}
