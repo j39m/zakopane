8689463e18507f3f64a6964e9bdc68f354add06b
j39m <hikaph+oss@gmail.com>
2021-05-02 01:44:53 -0700

    checksum: Begin ChecksumTaskManager
    This change provides some barebones sketches for the general layout of
    the ChecksumTaskManager struct.

--- src/checksum.rs
+++ src/checksum.rs
@@ -2,7 +2,36 @@
 
 use crate::structs::ZakopaneError;
 
-const MAX_TASKS: i32 = 8;
+const MAX_TASKS: usize = 8;
+
+#[derive(Default)]
+struct ChecksumTaskManager {
+    // Non-associative container enumerating paths and checksums.
+    sums: std::vec::Vec<(String, String)>,
+
+    // Map of outstanding checksum tasks.
+    // *    Keys: absolute paths being checksummed
+    // *    Values: tokio JoinHandles for checksum tasks
+    tasks: std::collections::HashMap<String, tokio::task::JoinHandle<String>>,
+}
+
+async fn do_checksum(path: std::path::PathBuf) -> Result<String, ZakopaneError> {
+    todo!()
+}
+
+impl ChecksumTaskManager {
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    pub async fn spawn_task(&self, path: std::path::PathBuf) {
+        assert!(
+            self.tasks.len() < MAX_TASKS,
+            "attempted to spawn too many tasks"
+        );
+        tokio::spawn(do_checksum(path));
+    }
+}
 
 async fn checksum_impl() -> Result<String, ZakopaneError> {
     // *    Walk the target path.
