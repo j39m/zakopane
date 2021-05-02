5226b1d03bf32e65764c6ebf8d4733b11a0b46e3
j39m <hikaph+oss@gmail.com>
2021-05-01 23:53:25 -0700

    libzakopane: Reorganize library
    This change
    *   moves the `compare()` functionality out of the libzakopane
        implementation file and
    *   reorganizes the various uses of `mod` and `use` (with and without
        `pub`) around the same.

---
+++ src/compare.rs
@@ -0,0 +1,69 @@
+use crate::config;
+use crate::snapshot;
+use crate::violations;
+
+// Compares the older snapshot against the newer snapshot, accruing
+// violations where discrepancies are detected per policy.
+fn check_modifications_and_deletions(
+    config: &config::Config,
+    older_snapshot: &snapshot::Snapshot,
+    newer_snapshot: &snapshot::Snapshot,
+    violations: &mut violations::Violations,
+) {
+    for (path, checksum) in older_snapshot.iter() {
+        let (_rule_repr, policy) = config.match_policy(path);
+        if policy == config::POLICY_IGNORE {
+            continue;
+        }
+
+        match newer_snapshot.get(path) {
+            Some(newer_checksum) => {
+                if (policy & config::POLICY_NOMODIFY) != 0 && checksum != newer_checksum {
+                    violations.add(path, violations::MODIFIED).unwrap();
+                }
+            }
+            None => {
+                if (policy & config::POLICY_NODELETE) != 0 {
+                    violations.add(path, violations::DELETED).unwrap();
+                }
+            }
+        }
+    }
+}
+
+// Compares the newer snapshot against the older snapshot, accruing
+// violations where discrepancies are detected per policy.
+fn check_additions(
+    config: &config::Config,
+    older_snapshot: &snapshot::Snapshot,
+    newer_snapshot: &snapshot::Snapshot,
+    violations: &mut violations::Violations,
+) {
+    for (path, _checksum) in newer_snapshot.iter() {
+        let (_rule_repr, policy) = config.match_policy(path);
+        if policy == config::POLICY_IGNORE {
+            continue;
+        }
+
+        match older_snapshot.get(path) {
+            Some(_older_checksum) => (),
+            None => {
+                if (policy & config::POLICY_NOADD) != 0 {
+                    violations.add(path, violations::ADDED).unwrap();
+                }
+            }
+        }
+    }
+}
+
+pub fn compare(
+    config: &config::Config,
+    older_snapshot: &snapshot::Snapshot,
+    newer_snapshot: &snapshot::Snapshot,
+) -> violations::Violations {
+    let mut violations = violations::Violations::new();
+    check_modifications_and_deletions(&config, &older_snapshot, &newer_snapshot, &mut violations);
+    check_additions(&config, &older_snapshot, &newer_snapshot, &mut violations);
+
+    violations
+}
--- src/libzakopane.rs
+++ src/libzakopane.rs
@@ -1,75 +1,8 @@
+mod compare;
 pub mod config;
 pub mod helpers;
 pub mod snapshot;
 pub mod structs;
 pub mod violations;
 
-use config::Config;
-use snapshot::Snapshot;
-use violations::Violations;
-
-// Compares the older snapshot against the newer snapshot, accruing
-// violations where discrepancies are detected per policy.
-fn check_modifications_and_deletions(
-    config: &Config,
-    older_snapshot: &Snapshot,
-    newer_snapshot: &Snapshot,
-    violations: &mut Violations,
-) {
-    for (path, checksum) in older_snapshot.iter() {
-        let (_rule_repr, policy) = config.match_policy(path);
-        if policy == config::POLICY_IGNORE {
-            continue;
-        }
-
-        match newer_snapshot.get(path) {
-            Some(newer_checksum) => {
-                if (policy & config::POLICY_NOMODIFY) != 0 && checksum != newer_checksum {
-                    violations.add(path, violations::MODIFIED).unwrap();
-                }
-            }
-            None => {
-                if (policy & config::POLICY_NODELETE) != 0 {
-                    violations.add(path, violations::DELETED).unwrap();
-                }
-            }
-        }
-    }
-}
-
-// Compares the newer snapshot against the older snapshot, accruing
-// violations where discrepancies are detected per policy.
-fn check_additions(
-    config: &Config,
-    older_snapshot: &Snapshot,
-    newer_snapshot: &Snapshot,
-    violations: &mut Violations,
-) {
-    for (path, _checksum) in newer_snapshot.iter() {
-        let (_rule_repr, policy) = config.match_policy(path);
-        if policy == config::POLICY_IGNORE {
-            continue;
-        }
-
-        match older_snapshot.get(path) {
-            Some(_older_checksum) => (),
-            None => {
-                if (policy & config::POLICY_NOADD) != 0 {
-                    violations.add(path, violations::ADDED).unwrap();
-                }
-            }
-        }
-    }
-}
-
-pub fn compare(
-    config: &Config,
-    older_snapshot: &Snapshot,
-    newer_snapshot: &Snapshot,
-) -> Violations {
-    let mut violations = Violations::new();
-    check_modifications_and_deletions(&config, &older_snapshot, &newer_snapshot, &mut violations);
-    check_additions(&config, &older_snapshot, &newer_snapshot, &mut violations);
-
-    violations
-}
+pub use compare::compare;
