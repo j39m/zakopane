808dbea7c0a77a713a5b5fd8f2bb5ed8f1dfd98a
j39m <hikaph+oss@gmail.com>
2021-05-02 19:23:44 -0700

    checksum: Add spawning and collecting
    This change roughly implements sketches of
    *   the digest worker tasks and
    *   the collection stage that collates the output of the same.

--- src/checksum.rs
+++ src/checksum.rs
@@ -3,23 +3,78 @@
 use crate::structs::ZakopaneError;
 
 const MAX_TASKS: usize = 8;
-type ChecksumResult = Result<String, ZakopaneError>;
 
+struct ChecksumWithPath {
+    checksum: String,
+    path: String,
+}
+
+impl ChecksumWithPath {
+    pub fn new(c: String, p: String) -> Self {
+        Self {
+            checksum: c,
+            path: p,
+        }
+    }
+}
+
+type ChecksumResult = Result<ChecksumWithPath, ZakopaneError>;
+
+// Thread-shared fields are wrapped in `Arc` to silence build complaints
+// about references (moved into spawned threads) outliving this struct.
 struct ChecksumTaskManager {
     // Non-associative container enumerating paths and checksums.
-    sums: std::vec::Vec<(String, String)>,
+    sums: std::vec::Vec<ChecksumWithPath>,
 
-    // Map of outstanding checksum tasks.
-    // *    Keys: absolute paths being checksummed
-    // *    Values: tokio JoinHandles for checksum tasks
-    tasks: std::collections::HashMap<String, tokio::task::JoinHandle<()>>,
+    // Rate-limiter for spawned checksum tasks.
+    semaphore: std::sync::Arc<tokio::sync::Semaphore>,
+
+    results_counter: std::sync::Arc<std::sync::atomic::AtomicUsize>,
 
     sender: tokio::sync::mpsc::Sender<ChecksumResult>,
     receiver: tokio::sync::mpsc::Receiver<ChecksumResult>,
 }
 
-async fn do_checksum(path: std::path::PathBuf, sender: tokio::sync::mpsc::Sender<ChecksumResult>) {
-    todo!()
+// Sends a checksum task result downstream to the collector task.
+async fn send_checksum_result(
+    result: ChecksumResult,
+    sender: tokio::sync::mpsc::Sender<ChecksumResult>,
+) {
+    if let Err(_) = sender.send(result).await {
+        eprintln!("BUG: Sender::send() failed");
+    }
+}
+
+// Separated from `do_checksum()` to ensure the `add_permits()` call is
+// always hit.
+async fn do_checksum_impl(
+    path: std::path::PathBuf,
+    sender: tokio::sync::mpsc::Sender<ChecksumResult>,
+) {
+    let contents = match crate::helpers::ingest_file(path.to_str().unwrap()) {
+        Ok(contents) => contents,
+        Err(e) => return send_checksum_result(Err(e), sender).await,
+    };
+    let checksum = crypto_hash::hex_digest(crypto_hash::Algorithm::SHA256, contents.as_ref());
+    send_checksum_result(
+        Ok(ChecksumWithPath::new(
+            checksum,
+            String::from(path.to_str().unwrap()),
+        )),
+        sender,
+    )
+    .await;
+}
+
+async fn do_checksum(
+    path: std::path::PathBuf,
+    sender: tokio::sync::mpsc::Sender<ChecksumResult>,
+    semaphore_clone: std::sync::Arc<tokio::sync::Semaphore>,
+) {
+    do_checksum_impl(path, sender).await;
+
+    // See comment in `ChecksumTaskManager::spawn_task()`.
+    semaphore_clone.add_permits(1);
 }
 
 impl ChecksumTaskManager {
@@ -27,25 +82,50 @@ impl ChecksumTaskManager {
         let (sender, receiver) = tokio::sync::mpsc::channel(MAX_TASKS);
         Self {
             sums: Default::default(),
-            tasks: Default::default(),
+            semaphore: std::sync::Arc::new(tokio::sync::Semaphore::new(MAX_TASKS)),
+            results_counter: std::sync::Arc::new(std::sync::atomic::AtomicUsize::new(0)),
             sender: sender,
             receiver: receiver,
         }
     }
 
-    pub fn spawn_task(&mut self, path: std::path::PathBuf) {
-        assert!(
-            self.tasks.len() < MAX_TASKS,
-            "attempted to spawn too many tasks"
-        );
-        self.tasks.insert(
-            String::from(path.to_str().unwrap()),
-            tokio::spawn(do_checksum(path, self.sender.clone())),
-        );
+    async fn spawn_task(&mut self, path: std::path::PathBuf) {
+        self.results_counter
+            .fetch_add(1, std::sync::atomic::Ordering::SeqCst);
+        let permit = self.semaphore.acquire().await.unwrap();
+
+        // Icky workaround for not being able to pass `SemaphorePermit`
+        // directly into a spawned task. Note that new permits are added
+        // later in the checksum task.
+        //
+        // See also: https://github.com/tokio-rs/tokio/issues/1998
+        permit.forget();
+
+        let sender = self.sender.clone();
+        let semaphore_clone = self.semaphore.clone();
+        tokio::task::spawn_blocking(move || do_checksum(path, sender, semaphore_clone));
+    }
+
+    async fn collect_checksum_results(&mut self) {
+        loop {
+            if let Some(result) = self.receiver.recv().await {
+                match result {
+                    Ok(digest_with_path) => self.sums.push(digest_with_path),
+                    Err(e) => panic!("BUG from checksum task: {:?}", e),
+                }
+            }
+            if self.sums.len()
+                == self
+                    .results_counter
+                    .load(std::sync::atomic::Ordering::SeqCst)
+            {
+                return;
+            }
+        }
     }
 }
 
-async fn checksum_impl() -> ChecksumResult {
+async fn checksum_impl() -> Result<String, ZakopaneError> {
     // *    Walk the target path.
     // *    If we can, then spawn a task, providing the current path
     //      in the walk and a cloned transmitter.
@@ -55,7 +135,7 @@ async fn checksum_impl() -> ChecksumResult {
 }
 
 #[allow(dead_code)]
-fn checksum() -> ChecksumResult {
+pub fn checksum() -> Result<String, ZakopaneError> {
     let runtime = tokio::runtime::Runtime::new().unwrap();
     runtime.block_on(checksum_impl())
 }
